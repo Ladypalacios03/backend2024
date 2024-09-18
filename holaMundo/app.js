1b9ecc808e1896d8b19ebf639f071b0070d7cb97
Lady Sthefany Palacios Cabrera <ladyrosa.86@gmail.com>
2024-09-18 11:41:00 -0600

    Commit inicial

---
+++ holaMundo/app.js
@@ -0,0 +1,4 @@
+//import { otroMensaje } from "./hola"
+const {otroMensaje}=require ("./hola")
+console.log("Este es el punto de entrada")
+console.log(otroMensaje)
---
+++ holaMundo/hola.js
@@ -0,0 +1,9 @@
+const mensaje ="HOLA MUNDO"
+const otroMensaje="Otro mensaje"
+console.log(mensaje)
+//console.log(otroMensaje)
+
+if(mensaje){
+    console.log("se imprimioo el mensaje")
+}
+module.export={otroMensaje}
