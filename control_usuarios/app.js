5afe639099b64c50783c632c792ddd92ac7aa030
Lady Sthefany Palacios Cabrera <ladyrosa.86@gmail.com>
2024-10-07 12:02:03 -0600

    Modificaciones 2

--- control_usuarios/app.js
+++ control_usuarios/app.js
@@ -1,24 +1,41 @@
 const express=require("express");
 
 const app=express();
+const usuarios=[
+    {
+        id: 1,
+        nombre: "Lady",
+        apellido: "Palacios",
+        email: "ladyrosa.86@gmail.com"
+    },
+    {
+        id: 2,
+        nombre: "Agustin",
+        apellido: "Palacios",
+        email: "ladyrosa.86@gmail.com"
+    },
+];
 
 app.get("/usuarios", (req, res)=> {
-    const usuarios=[
-        {
-            id: 1,
-            nombre: "Lady",
-            apellido: "Palacios",
-            email: "ladyrosa.86@gmail.com"
-        },
-        {
-            id: 2,
-            nombre: "Agustin",
-            apellido: "Palacios",
-            email: "ladyrosa.86@gmail.com"
-        },
-    ];
-    res.status(200).send(usuarios);
+    res.status(200).send({usuarios});
 })//obtener informacion
+
+app.get("/usuarios/1",(req, res)=>{
+    res.status(200).send(usuarios[0]);
+
+});
+
+
+app.get("/usuarios/:id",(req, res)=>{
+    const {id}=req.params;
+    //const params = req.params;
+    //console.log(params);
+   const usuario= usuarios.find((usuario)=>usuario.id === +id);
+
+    //res.status(200).send("probando");
+    res.status(200).send(usuario);
+
+});
 
 app.listen(3000, ()=>{
     console.log("Servidor corriendo en http://localhost:3000")
