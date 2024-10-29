9dfdbadb97ab0baabc3abc9b2cda711855464550
Lady Sthefany Palacios Cabrera <ladyrosa.86@gmail.com>
2024-10-29 07:55:50 -0600

    Tarea

--- stock_manager/src/controllers/users.js
+++ stock_manager/src/controllers/users.js
@@ -29,4 +29,71 @@ const getById=(req=request, res=response)=>{
 
 }
 
-module.exports={getAll, getById};
+// TAREA
+// Crear un nuevo usuario
+const createUser = (req = request, res = response) => {
+  const { name } = req.body;
+
+  if (!name) {
+      res.status(400).send('Name is required');
+      return;
+  }
+
+  const newUser = {
+      id: users.length + 1,
+      name
+  };
+
+  users.push(newUser);
+  res.status(201).send('El usuario se creo correctamente');
+};
+
+// Actualizar un usuario por ID
+const updateUser = (req = request, res = response) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  if (isNaN(id)) {
+      res.status(400).send('Invalid ID');
+      return;
+  }
+
+  const user = users.find(user => user.id === +id);
+
+  if (!user) {
+      res.status(404).send('User not found');
+      return;
+  }
+
+  if (!name) {
+      res.status(400).send('Name is required');
+      return;
+  }
+
+  user.name = name;
+  res.send('El usuario se actualizo');
+};
+
+// Eliminar un usuario por ID
+const deleteUser = (req = request, res = response) => {
+  const { id } = req.params;
+
+  if (isNaN(id)) {
+      res.status(400).send('Invalid ID');
+      return;
+  }
+
+  const index = users.findIndex(user => user.id === +id);
+
+  if (index === -1) {
+      res.status(404).send('User not found');
+      return;
+  }
+
+  users.splice(index, 1);
+  res.status(204).send('El usuario se elimino');
+};
+
+module.exports = { getAll, getById, createUser, updateUser, deleteUser };
+
+//anterior: module.exports={getAll, getById};
--- stock_manager/src/routes/users.js
+++ stock_manager/src/routes/users.js
@@ -1,11 +1,18 @@
 const{Router}=require('express');
-const{getAll, getById}=require('../controllers/users');
+const { getAll, getById, createUser, updateUser, deleteUser } = require('../controllers/users');
+
+//anterior : const{getAll, getById}=require('../controllers/users');
 
 const router=Router();
 
 router.get('/',getAll);
 
 router.get('/:id',getById);
+
+//tarea
+router.post('/', createUser);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
 
 module.exports=router;
