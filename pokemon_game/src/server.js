f1567ea259f7119d77d049adf4b43ea31869532c
Lady Sthefany Palacios Cabrera <ladyrosa.86@gmail.com>
2024-12-02 12:03:59 -0600

    Clase 02/12/24

--- pokemon_game/src/server.js
+++ pokemon_game/src/server.js
@@ -1,5 +1,7 @@
 const express=require('express');
 
+const usersRoutes=require('./routes/users');
+
 class Server{
     constructor(){
     this.app=express();
@@ -13,6 +15,7 @@ class Server{
     }
 
     routes(){
+        this.app.use('/users',usersRoutes);
     }
 
     start(){
